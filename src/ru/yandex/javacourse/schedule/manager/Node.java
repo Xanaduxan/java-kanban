6b14590aa5a98040bea6aa2da19f7bcd1dd97019
Olga Karpova <oevkarpova@gmail.com>
2025-10-15 01:03:12 +0300

    fix after code-review

--- src/ru/yandex/javacourse/schedule/manager/Node.java
+++ src/ru/yandex/javacourse/schedule/manager/Node.java
@@ -1,33 +1,33 @@
 package ru.yandex.javacourse.schedule.manager;
 
-final class Node<E> {
+public final class Node<E> {
     private final E data;
     private Node<E> next;
     private Node<E> prev;
 
-    Node(Node<E> prev, E data, Node<E> next) {
+    public Node(Node<E> prev, E data, Node<E> next) {
         this.prev = prev;
         this.data = data;
         this.next = next;
     }
 
-    E getData() {
+    public E getData() {
         return data;
     }
 
-    Node<E> getNext() {
+    public Node<E> getNext() {
         return this.next;
     }
 
-    Node<E> getPrev() {
+    public Node<E> getPrev() {
         return this.prev;
     }
 
-    void setPrev(Node<E> prev) {
+    public void setPrev(Node<E> prev) {
         this.prev = prev;
     }
 
-    void setNext(Node<E> next) {
+    public void setNext(Node<E> next) {
         this.next = next;
     }
 
